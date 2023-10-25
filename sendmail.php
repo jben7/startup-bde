f76162e8c0ee56ce4000b0d889dd752d2eafec39
jben7 <{ID}+{username}@users.noreply.github.com>
2023-10-25 09:14:29 +0200

    sendmail php finito

--- sendmail.php
+++ sendmail.php
@@ -1,35 +1,31 @@
 <?php
 
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     if(
-//         !empty($_POST['name'])
-//         && !empty($_POST['email'])
-//         && !empty($_POST['message'])
-//     ){
-//         $name = $_POST["name"];
-//         $email = $_POST["email"];
-//         $message = $_POST["message"];
-
-
-//         $to = "jadbenlatreche@gmail.com";
-//         $subject = "Nouvelle demande de contact";
-//         $body = "Nom: {$name}\nEmail: {$email}\nTelephone: {$phone}\nMessage: {$message}";
-//         $headers = "De: {$email}";
-
-
-//         if (mail($to, $subject, $body, $headers)) {
-//             echo "Message sent successfully!";
-//         } else {
-//             echo "Failed to send message.";
-//         }
-//     }
-// }
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (
+        !empty($_POST['name'])
+        && !empty($_POST['email'])
+        && !empty($_POST['message'])
+    ) {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $message = $_POST["message"];
 
-    $monMail = mail("jadbenlatreche@gmail.com", "subject", "body", "");
 
-    echo $monMail;
-};
+        $to = "jadbenlatreche@gmail.com";
+        $subject = "Nouvelle demande de contact";
+        $body = "Nom: {$name}\nEmail: {$email}\nTelephone: {$phone}\nMessage: {$message}";
+        $headers = "";
 
-?>
+
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Message sent successfully!";
+            sleep(3);
+            header("Location: /contact.html");
+            die();
+        } else {
+            echo "Failed to send message.";
+            sleep(3);
+            header("Location: /contact.html");
+        }
+    }
+}
