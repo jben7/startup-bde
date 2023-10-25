2f642f1d81640ac853e2e33e274479600c9b3953
jben7 <{ID}+{username}@users.noreply.github.com>
2023-10-25 09:17:37 +0200

    fix sendmail1

--- sendmail.php
+++ sendmail.php
@@ -1,31 +1,26 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (
-        !empty($_POST['name'])
-        && !empty($_POST['email'])
-        && !empty($_POST['message'])
-    ) {
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $message = $_POST["message"];
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
 
-        $to = "jadbenlatreche@gmail.com";
-        $subject = "Nouvelle demande de contact";
-        $body = "Nom: {$name}\nEmail: {$email}\nTelephone: {$phone}\nMessage: {$message}";
-        $headers = "";
+    $to = "jadbenlatreche@gmail.com";
+    $subject = "Nouvelle demande de contact";
+    $body = "Nom: {$name}\nEmail: {$email}\nTelephone: {$phone}\nMessage: {$message}";
+    $headers = "";
 
-
-        if (mail($to, $subject, $body, $headers)) {
-            echo "Message sent successfully!";
-            sleep(3);
-            header("Location: /contact.html");
-            die();
-        } else {
-            echo "Failed to send message.";
-            sleep(3);
-            header("Location: /contact.html");
-        }
+    $monMail = mail($to, $subject, $body, $headers);
+    if ($monMail == 1) {
+        echo "Message sent successfully!";
+        sleep(3);
+        header("Location: /contact.html");
+        die();
+    } else {
+        echo "Failed to send message.";
+        sleep(3);
+        header("Location: /contact.html");
     }
 }
