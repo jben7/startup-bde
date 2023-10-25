6c47ee39dd0015b14a1359bd19573e574120edae
jben7 <{ID}+{username}@users.noreply.github.com>
2023-10-25 09:25:44 +0200

    fix sendmail3

--- sendmail.php
+++ sendmail.php
@@ -4,24 +4,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $sujet = $_POST["subject"];
     $message = $_POST["message"];
 
 
     $to = "jadbenlatreche@gmail.com";
     $subject = "Nouvelle demande de contact";
-    $body = "Nom: {$name}\nEmail: {$email}\nTelephone: {$phone}\nMessage: {$message}";
+    $body = "Nom: {$name} \nEmail: {$email} \nSujet: {$sujet} \nMessage: {$message}";
     $headers = "";
 
     $monMail = mail($to, $subject, $body, $headers);
-    echo $monMail;
-    // if ($monMail == 1) {
-    //     echo "Message sent successfully!";
-    //     sleep(3);
-    //     header("Location: /contact.html");
-    //     die();
-    // } else {
-    //     echo "Failed to send message.";
-    //     sleep(3);
-    //     header("Location: /contact.html");
-    // }
+    if ($monMail) {
+        echo "Message sent successfully!";
+        sleep(3);
+        header("Location: /contact.html");
+        die();
+    } else {
+        echo "Failed to send message.";
+        sleep(3);
+        header("Location: /contact.html");
+    }
 }
